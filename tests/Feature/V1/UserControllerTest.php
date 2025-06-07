99aec72abb4bba8e0efe197f529c34d1370877e6
NicoDevel <nicolassabogal85@gmail.com>
2025-06-07 01:28:06 -0500

    User test

--- tests/Feature/V1/UserControllerTest.php
+++ tests/Feature/V1/UserControllerTest.php
@@ -25,10 +25,73 @@ class UserControllerTest extends TestCase
         $response->assertCreated();
 
         $this->assertDatabaseHas('users', [
-            'id' => 1,
+            'name' => $userData['name'],
+            'email' => $userData['email'],
+            'role_id' => Role::USER_ID
+        ]);
+
+
+        $userData = [
+            ...User::factory()->make()->toArray(),
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'role_id' => Role::ADMIN_ID
+        ];
+
+        $response = $this->post(route('v1.user.register'), $userData);
+
+        $response->assertCreated();
+
+        $this->assertDatabaseHas('users', [
             'name' => $userData['name'],
             'email' => $userData['email'],
             'role_id' => Role::USER_ID
         ]);
     }
+
+    public function testRegisterNewUserWithSession(): void
+    {
+        $user = User::factory()->make([
+            'role_id' => Role::ADMIN_ID
+        ]);
+
+        $this->actingAs($user);
+
+        //try to create a user role
+        $userData = [
+            ...User::factory()->make(['role_id' => Role::USER_ID])->toArray(),
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $response = $this->post(route('v1.user.register'), $userData);
+
+        $response->assertCreated();
+
+        $this->assertDatabaseHas('users', [
+            'name' => $userData['name'],
+            'email' => $userData['email'],
+            'role_id' => Role::USER_ID
+        ]);
+
+        // Try to create admin user
+
+        $userData = [
+            ...User::factory()->make(['role_id' => Role::ADMIN_ID])->toArray(),
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $response = $this->post(route('v1.user.register'), $userData);
+
+        $response->assertCreated();
+
+        $this->assertDatabaseHas('users', [
+            'name' => $userData['name'],
+            'email' => $userData['email'],
+            'role_id' => Role::ADMIN_ID
+        ]);
+
+    }
+
 }
